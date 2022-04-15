2c3c833558037b5691e046bffddb7d96df7ed479
Redwi-v <forest45456@gmail.com>
2022-04-15 08:39:32 +0300

    added_close_beta

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,8 @@
 const { app, BrowserWindow } = require('electron');
+
 const path = require('path');
 const isDev = require('electron-is-dev');
-
+require('@electron/remote/main').initialize();
 
 const createWindow = () => {
 	// Create the browser window.
@@ -14,8 +15,11 @@ const createWindow = () => {
 			preload: path.join(__dirname, 'preload.js'),
 			nodeIntegration: true,
 			contextIsolation: false,
+			enableRemoteModule: true,
 		},
 	});
+
+	require('@electron/remote/main').enable(mainWindow.webContents);
 
 	// and load the index.html of the app.
 	mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
@@ -44,6 +48,4 @@ app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') app.quit();
 });
 
- const closeWindow = () => {
-	remote.getCurrentWindow().close()
-}
+// костыль--
--- src/components/ManiPage/MainPage.jsx
+++ src/components/ManiPage/MainPage.jsx
@@ -1,16 +1,22 @@
 import { NavLink } from 'react-router-dom';
-import style from './mainPage.module.scss'
+import style from './mainPage.module.scss';
 
 const MainPage = props => {
 	return (
 		<div className='container'>
 			<menu className={style.menu}>
-				<NavLink className={style.link} to='game'>game</NavLink>
-				<NavLink className={style.link} to='about'>About</NavLink>
-				<NavLink onClick={() => {
-					console.log('close')
-				}} className={style.link} to=''>Exit</NavLink>
-			</menu> 
+				<NavLink className={style.link} to='game'>
+					game
+				</NavLink>
+				<NavLink className={style.link} to='about'>
+					About
+				</NavLink>
+				<NavLink className={style.link} to=''>
+					Exit
+				</NavLink>
+
+				<button id='close'>close</button>
+			</menu>
 		</div>
 	);
 };
